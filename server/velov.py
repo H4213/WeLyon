10474b73f687de6126be1aef335027016c606af8
Toumassa <artogu@live.fr>
2015-04-28 16:12:19 +0200

    Velov Add/Refresh compatible

--- server/velov.py
+++ server/velov.py
@@ -13,11 +13,13 @@ from src.model import Velov
 #ex: "https://download.data.grandlyon.com/ws/rdata/jcd_jcdecaux.jcdvelov/all.json"
 
 def refreshVelovData(urlSource) :
+	print("[LOG] Loading of json from the server Velov ")
 	urllib.urlretrieve(urlSource, "velov.json")
 	json_file = open('velov.json')
 
 	data = json.load(json_file)
 	listVelov = []
+	print("[LOG] Parsing the json file")
 	for i in  range(0, data["nb_results"]):
 		idUser = 1
 		title = "Velov de " + data["values"][i][2]
@@ -31,6 +33,7 @@ def refreshVelovData(urlSource) :
 		obj = Velov(title, lnd, lat, idUser, [], description)
 		obj.libre = data["values"][i][12]
 		obj.velo = data["values"][i][13]
+		obj.idVelov = data["values"][i][17]
 		listVelov.append(obj);
 
 	json_file.close()
@@ -39,5 +42,8 @@ def refreshVelovData(urlSource) :
 	return listVelov
 	
 list = refreshVelovData("https://download.data.grandlyon.com/ws/rdata/jcd_jcdecaux.jcdvelov/all.json");
+
+print("[LOG] Updating the database")
 for v in list:
-	service.addPin(v)
+	service.updateVelovByIdVelov(v)
+print("[LOG] Velo'v is up to date")
