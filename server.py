d71ae6c7f48a9811d198923f417a2e5eac8bb84e
Toumassa <artogu@live.fr>
2015-04-29 09:41:40 +0200

    Timer ++

--- server.py
+++ server.py
@@ -3,6 +3,7 @@
 
 #constants
 VELOV_DATA_SOURCE = "https://download.data.grandlyon.com/ws/rdata/jcd_jcdecaux.jcdvelov/all.json"
+VELOV_DATA_REFRESH_INTERVAL = 60.0
 
 import sys
 reload(sys)
@@ -147,13 +148,21 @@ def displaye():
 def page_not_found(error):
     return jsonify(error="404"), 404
 	
-def start_velov_data():
-	t = threading.Timer(60.0, velov.refreshVelovData, [VELOV_DATA_SOURCE])
-	t.start()
+
+#lance le rafraichissement periodique des données velov
+def start_velov_data(tempo = 60.0):
+	velov.refreshVelovData(VELOV_DATA_SOURCE)
+	threading.Timer(tempo, start_velov_data, [tempo]).start()
 	
 
 if __name__ == '__main__':
-  app.debug = True
-  app.run()
+	
+	print("[LOG] Démarrage du serveur")
+	#app.debug = True
+	app.run()
+	
+	
+	print("[LOG] Lancement du rafraichissement des donnees Velov")
+	start_velov_data(VELOV_DATA_REFRESH_INTERVAL)
   #port = int(os.environ.get("PORT", 5000))
   #app.run(host='0.0.0.0', port=port)
